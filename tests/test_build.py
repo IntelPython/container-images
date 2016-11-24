7b740c3bde736e3da29c85f7b52337ce8f48088f
Robert Cohn <Robert.S.Cohn@intel.com>
2016-11-24 09:33:15 -0600

    idp2/3_core

---
+++ tests/test_build.py
@@ -0,0 +1,19 @@
+import getpass
+import os
+import subprocess
+
+def get_proxies():
+    proxies = ''
+    for var in ['http_proxy','https_proxy','no_proxy']:
+        if var in os.environ:
+            proxies += ' --build-arg %s=%s' % (var,os.environ[var])
+    return proxies
+
+def do_build(dir):
+    tag = '%s/%s' % (getpass.getuser(),dir)
+    subprocess.check_call('docker build %s -t %s --file %s/Dockerfile .' % (get_proxies(),tag,dir),shell=True)
+    subprocess.check_call('docker run -t %s python -c 1' % tag ,shell=True)
+
+def test_builds():
+    for dir in ['idp2_core','idp3_core']:
+        do_build(dir)
